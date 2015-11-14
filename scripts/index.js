a85054d73d3075e4692311b02da84c3c1485c722
androiddenemeleri <androiddenemeleri@gmail.com>
2015-11-14 23:06:36 +0200

    zipli

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        var page2button = document.getElementById("gotopage2")
+        if (page2button) {
+            page2button.addEventListener("click", function () {
+                window.location.href = "page2.html";
+            });
+        }
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+})();
